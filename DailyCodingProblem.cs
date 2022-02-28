c9a0d6c07c754f3e0fd952baa667cce62e277b12
giacomo.satta <giacomo.satta@WINAPPW3CODDICP>
2022-02-28 16:11:22 +0100

    5° problema aggiunto

--- DailyCodingProblem.cs
+++ DailyCodingProblem.cs
@@ -134,7 +134,7 @@ void CheckLowestPositive(int[] arr){
 // to count number of decodings that
 // can be formed from a given digit sequence
 char[] digits = { '1', '2', '3', '4' };
-DecodeNumber(digits);
+// DecodeNumber(digits);
 void DecodeNumber(char[] digits) {
 	int n = digits.Length;
 	Console.Write("Count is ");
@@ -180,4 +180,25 @@ static int countDecoding(char[] digits, int n)
 
 #endregion
 
+#region AutoCompleteSystem
+//given a query string s and a set of all possible query strings, return all strings in the set that have s as a prefix.
+//For example, given the query string de and the set of strings [dog, deer, deal], return [deer, deal].
+//Asked by Twitter
 
+string s = "de";
+string[] arrayStrings = {"dog","deer","deal"};
+CheckTheString(s,arrayStrings);
+
+void CheckTheString(string s, string[] arrayStrings)
+{
+    List<string> output = new List<string>();
+    foreach(var str in arrayStrings)
+    {
+        if (str.StartsWith(s))
+            output.Add(str);
+    }
+
+    Console.WriteLine(string.Join(",",output));
+}
+
+#endregion
