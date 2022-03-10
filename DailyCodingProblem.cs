c03f78e6de4f9f9d5aeeb1aebadeebe3607ec40a
giacomo.satta <giacomo.satta@WINAPPW3CODDICP>
2022-03-10 18:45:17 +0100

    ReconstructThePhrase Solved

--- DailyCodingProblem.cs
+++ DailyCodingProblem.cs
@@ -210,7 +210,7 @@ void CheckTheString(string s, string[] arrayStrings)
 
 int[] list1 = {3,7,8,10};
 int[] list2 = {99,1,5,7};
-FindTheIntersect(list1,list2);
+// FindTheIntersect(list1,list2);
 
 void FindTheIntersect(int[] list1, int[] list2){
     int? output = null;
@@ -228,4 +228,40 @@ void FindTheIntersect(int[] list1, int[] list2){
     if(output != 0)
     Console.WriteLine($"Le due liste si intersecano con il valore {output}");
 }
+#endregion
+
+#region ReconstructThePhrase
+//Given a dictionary of words and a string made up of those words (no spaces), return the original sentence in a list. 
+//If there is more than one possible reconstruction, return any of them. If there is no possible reconstruction, then return null.
+//Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and the string "bedbathandbeyond", return either ['bed', 'bath', 'and', 'beyond] 
+//or ['bedbath', 'and', 'beyond'].
+//Asked by Microsoft
+
+string[] words = {"bed","bath","bedbath","and","beyond"};
+string phrase = "ganggang";
+ReconstructThePhrase(words,phrase); 
+
+void ReconstructThePhrase(string[] words,string phrase){
+    List<string> output = new List<string>();
+
+    foreach(string word in words){
+        if(phrase.Contains(word)){
+            phrase = phrase.Replace(word,"");
+            output.Add(word);
+            continue;
+        }
+    }
+
+    if(output.Count == 0)
+    {
+        Console.WriteLine("NULL");
+        return;
+    }
+
+    foreach(string word in output){
+        Console.WriteLine(word);
+    }
+
+}
+
 #endregion
